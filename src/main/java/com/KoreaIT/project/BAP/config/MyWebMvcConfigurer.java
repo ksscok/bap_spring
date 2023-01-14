aaee83c5aeefa5d65aa3dbcd6eaf87a8e3c61f17
ksscok <ksscok1@gmail.com>
2023-01-15 02:41:53 +0900

    로그인과 로그아웃 인터셉터에 페이지 추가

--- src/main/java/com/KoreaIT/project/BAP/config/MyWebMvcConfigurer.java
+++ src/main/java/com/KoreaIT/project/BAP/config/MyWebMvcConfigurer.java
@@ -3,12 +3,14 @@ package com.KoreaIT.project.BAP.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import com.KoreaIT.project.BAP.interceptor.BeforeActionInterceptor;
 import com.KoreaIT.project.BAP.interceptor.NeedLoginInterceptor;
+import com.KoreaIT.project.BAP.interceptor.NeedLogoutInterceptor;
 
 @Configuration
 public class MyWebMvcConfigurer implements WebMvcConfigurer {
@@ -19,6 +21,10 @@ public class MyWebMvcConfigurer implements WebMvcConfigurer {
 	// needLoginInterceptor 인터셉터 불러오기
 	@Autowired
 	NeedLoginInterceptor needLoginInterceptor;
+	
+	// needLogoutInterceptor 인터셉터 불러오기
+	@Autowired
+	NeedLogoutInterceptor needLogoutInterceptor;
 	
 	@Value("${custom.genFileDirPath}")
 	private String genFileDirPath;
@@ -32,9 +38,30 @@ public class MyWebMvcConfigurer implements WebMvcConfigurer {
 	// 이 함수는 인터셉터를 적용하는 역할을 합니다.
 	@Override
 	public void addInterceptors(InterceptorRegistry registry) {
-		registry.addInterceptor(beforeActionInterceptor).addPathPatterns("/**").excludePathPatterns("/resource/**")
-				.excludePathPatterns("/error");
-
-		registry.addInterceptor(needLoginInterceptor).addPathPatterns("/usr/member/doLogout");
+		InterceptorRegistration ir;
+		
+		ir = registry.addInterceptor(beforeActionInterceptor);
+		ir.addPathPatterns("/**");
+		ir.excludePathPatterns("/resource/**");
+		ir.excludePathPatterns("/error");
+		
+		// 로그인이 필요한 페이지를 추가하면 됩니다.
+		ir = registry.addInterceptor(needLoginInterceptor);
+		ir.addPathPatterns("/usr/member/myPage");
+		ir.addPathPatterns("/usr/member/checkPassword");
+		ir.addPathPatterns("/usr/member/doCheckPassword");
+		ir.addPathPatterns("/usr/member/modify");
+		ir.addPathPatterns("/usr/member/doModify");
+		
+		// 로그아웃이 필요한 페이지를 추가하면 됩니다.
+		ir = registry.addInterceptor(needLogoutInterceptor);
+		ir.addPathPatterns("/usr/member/login");
+		ir.addPathPatterns("/usr/member/doLogin");
+		ir.addPathPatterns("/usr/member/join");
+		ir.addPathPatterns("/usr/member/doJoin");
+		ir.addPathPatterns("/usr/member/findLoginId");
+		ir.addPathPatterns("/usr/member/doFindLoginId");
+		ir.addPathPatterns("/usr/member/findLoginPw");
+		ir.addPathPatterns("/usr/member/doFindLoginPw");
 	}
 }
