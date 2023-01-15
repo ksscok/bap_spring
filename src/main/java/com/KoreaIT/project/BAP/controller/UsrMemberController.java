7274c41945048975eacaacdce48f15b36a9808fc
ksscok <ksscok1@gmail.com>
2023-01-15 19:47:45 +0900

    비밀번호 암호화가 필요한곳 수정

--- src/main/java/com/KoreaIT/project/BAP/controller/UsrMemberController.java
+++ src/main/java/com/KoreaIT/project/BAP/controller/UsrMemberController.java
@@ -114,7 +114,7 @@ public class UsrMemberController {
 		if(Ut.empty(loginPw)) {
 			return rq.jsHistoryBack("비밀번호를 입력해주세요.");
 		}
-		if(rq.getLoginedMember().getLoginPw().equals(loginPw) == false) {
+		if(rq.getLoginedMember().getLoginPw().equals(Ut.sha256(loginPw)) == false) {
 			return rq.jsHistoryBack("비밀번호가 일치하지 않습니다.");
 		}
 		
